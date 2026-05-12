1f82e916b3e6badf42f86a6f6a4e6d3cc2f600af
vishnucanada <vishnucanada@gmail.com>
2026-05-12 17:14:54 -0400

    test pipeline

--- world_model/__init__.py
+++ world_model/__init__.py
@@ -1,5 +1,18 @@
 from .physics import World, Body, Wall, vec2
 from .render import Renderer
 from .env import PhysicsEnv
+from .dataset import SceneSpec, generate
+from .model import DynamicsMLP, load as load_model
 
-__all__ = ["World", "Body", "Wall", "vec2", "Renderer", "PhysicsEnv"]
+__all__ = [
+    "World",
+    "Body",
+    "Wall",
+    "vec2",
+    "Renderer",
+    "PhysicsEnv",
+    "SceneSpec",
+    "generate",
+    "DynamicsMLP",
+    "load_model",
+]
